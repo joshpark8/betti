2735a93bf387fa6735e74e58f8d1fcbfa20f2114
Josh Park <84344359+joshpark8@users.noreply.github.com>
2026-01-25 22:16:29 -0500

    rework how ideals are saved and generated

--- generate.m2
+++ generate.m2
@@ -3,33 +3,45 @@ needsPackage "RandomIdeals"
 needsPackage "BoijSoederberg"
 needsPackage "JSON"
 
--- file open
-f = "generated/ideals.json" << ""
+-- number of variables
+varCount = 4
+
+-- number of generators
+genCount = 3
+
+-- generate uniquely identifiable file in format "ideals[# of variables],[# of generators]-[index].json"
+fileIndex = 1
+filepath = "generated/ideals"
+fileID = concatenate(toString varCount, ",", toString genCount)
+while fileExists concatenate(filepath, fileID, "-", toString fileIndex, ".json") do (
+    fileIndex += 1
+)
+
+f = concatenate(filepath, fileID, "-", toString fileIndex, ".json") << ""
 
 -- define ring
-R = (ZZ/101)[x,y]
+R = (ZZ/101)[x_1..x_(varCount)]
+-- R = QQ[x,y,z]
 
 -- define mutable hash table to pair (grobner bases of) ideals with respective betti tables
-x = new MutableHashTable
+mhash = new MutableHashTable
 
---
-getNewIdeal = method()
-getNewIdeal(MutableHashTable) := x -> (
-    I = randomMonomialIdeal({random(50,150), random(50,150)},R);
-    Istr = toString gens gb I;
-    if not x#?Istr then I else getNewIdeal(x)
+-- define list of generator grades
+
+L = new MutableList
+for i from 1 to genCount do (
+    L#(i-1) = random(50,100)
 )
 
 -- generate ideals and match to betti tables
-for i from 1 to 1000 do (
-    I = getNewIdeal(x);
-
+elapsedTime for i from 1 to 1000 do (
+    I = randomMonomialIdeal({random(50,100),random(50,100),random(50,100)}, R);
     Istr = toString gens gb I;
-    x#Istr = toString matrix betti I;
+    mhash#Istr = toString matrix betti I;
 )
 
-hashx = new HashTable from x
+finalhash = new HashTable from mhash;
 
-f << toJSON hashx
+f << toJSON finalhash
 
 f << close
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import ast
 from typing import Iterable, List
 import json
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 
 
 def monomial_to_expvec(m: str, variables: Iterable[str] = ("x", "y", "z")) -> List[int]:
@@ -53,9 +50,7 @@ def str_to_matrix(ideal_str: list[str]):
     :param ideal_str: Description
     :type ideal_str: str
     """
-    return list(
-        monomial_to_expvec(generator, variables=("x", "y")) for generator in ideal_str
-    )
+    return list(monomial_to_expvec(generator) for generator in ideal_str)
 
 
 def brace_string_to_list(s: str):
@@ -78,54 +73,3 @@ def pad_with_mask(B: list[int], H: int):
     out[:h, :] = B
     mask[:h, :] = 1.0
     return np.concatenate([out.ravel(), mask.ravel()])  # length 4H
-
-
-if __name__ == "__main__":
-    FILEPATH = "generated/ideals.json"
-    try:
-        with open(FILEPATH, "r", encoding="utf8") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        print(f"Error: The file '{FILEPATH}' was not found.")
-    except json.JSONDecodeError as e:
-        print(f"Error: Failed to decode JSON from {FILEPATH}: {e}")
-
-    ideal_vectors = list()
-    betti_matrices = list()
-    for key in data:
-        ideal = key[9:-2].split(",")
-        # print(key[9:-2])
-        ideal_mtx = str_to_matrix(ideal)
-        ideal_vectors.append(ideal_mtx)
-        # print(ideal_mtx)
-
-        betti = data[key][7:]
-        betti_mtx = brace_string_to_list(betti)
-        betti_matrices.append(betti_mtx)
-        # print(betti_mtx)
-
-    # make betti tables uniform size
-    max_height = max(len(x) for x in betti_matrices)
-    Y = np.stack([pad_with_mask(B, max_height) for B in betti_matrices])
-
-    # append true height of table to ideal data
-    X_base = np.asarray(ideal_vectors, dtype=float).reshape(len(ideal_vectors), -1)
-
-    # Y: list of (height_i, 2) betti tables
-    heights = np.array(
-        [np.asarray(B).shape[0] for B in betti_matrices], dtype=float
-    ).reshape(-1, 1)
-
-    # add betti table dimension data to ideal vectors
-    X = np.concatenate([X_base, heights], axis=1)  # (n, 5)
-
-    # split into training/testing sets
-    Xtr, Xte, Ytr, Yte = train_test_split(X, Y, test_size=0.2, random_state=0)
-
-    regressor = RandomForestRegressor(n_estimators=500, random_state=0, oob_score=True)
-    regressor.fit(X, Y)
-
-    Yhat = regressor.predict(Xte)
-
-    print("test MSE:", mean_squared_error(Yte, Yhat))
-    print("test R2 :", r2_score(Yte, Yhat))
